39ad467bbb5e48a5c14eedb6adc267e46d15b02c
kurehajime <xiidec@gmail.com>
2022-04-10 13:42:56 +0000

    ４章までやった

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class QuotesTest < ApplicationSystemTestCase
 
   setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+    @quote = Quote.ordered.first
   end
 
   test "quoteを表示" do
